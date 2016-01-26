1c8dc19ed780bda7073b93eeb851ec6f6727a637
Mathias Bylund <materbyl@gmail.com>
2016-01-26 18:03:49 +0100

    Added GUI mechanics so that they now respond to the pod geometry

---
+++ Assets/GUI/Scripts/AltitudeText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class AltitudeText : MonoBehaviour {
+
+    public Rigidbody target;
+
+    private Text txt;
+    // "Hack" to make sure that altitude is 0 when on the ground, should be easy to make it work relative to the pod
+    private float offset = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        txt = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        txt.text = Convert.ToString(target.position.y - offset);
+    }
+}
---
+++ Assets/GUI/Scripts/VelocityText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class VelocityText : MonoBehaviour {
+
+    public Rigidbody target;
+
+    private Text txt;
+
+	// Use this for initialization
+	void Start () {
+        txt = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        txt.text = Convert.ToString(target.velocity.y);
+	}
+}
