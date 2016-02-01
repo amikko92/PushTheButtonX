3e05fabf165676c49f48f9292f45f68860d8c58e
Andreas <a.mikko92@gmail.com>
2016-02-01 15:26:03 +0100

    2D physics

--- Assets/Scripts/DropPod.cs
+++ Assets/Scripts/DropPod.cs
@@ -3,44 +3,33 @@ using System.Collections;
 
 public class DropPod : MonoBehaviour 
 {
-    //[SerializeField]
-    //private float m_thrustForce;
-    //
-    //[SerializeField]
-    //private float m_maxThrustForce;
-    //
-    //[SerializeField]
-    //private float m_accelerationTime = 1.0f;
-    //
-    //private float m_acceleration;
-    //private float m_accelerationFactor;
-
     [SerializeField]
     Thruster m_thruster;
 
-    private Rigidbody m_rigidbody;
+    private Rigidbody2D m_rigidbody2D;
 
-    private RayShooter m_rayShooter;
+    private RayShooter2D m_rayShooter2D;
 
     [SerializeField]
     private LayerMask m_groundMask;
 
 	private void Awake() 
 	{
-        m_rigidbody = GetComponent<Rigidbody>();
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
         m_thruster = GetComponent<Thruster>();
-        //m_accelerationFactor = 1.0f / m_accelerationTime;
-
-        m_rayShooter = new RayShooter();
-        m_rayShooter.Shoot(transform.position, Vector3.down, m_groundMask.value);
-        if(m_rayShooter.Hit())
-        {
-            Debug.Log(m_rayShooter.IntersectionDistance());
-        }
 	}
 	
 	private void FixedUpdate()
 	{
-        m_rigidbody.AddForce(m_thruster.ThrustForce());
+        m_rayShooter2D.Shoot(transform.position, Vector2.down);
+        if(m_rayShooter2D.Hit)
+        {
+
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            m_rigidbody2D.AddForce(m_thruster.ThrustForce());
+        }
     }
 }
--- Assets/Scripts/ThrusterConstantForce.cs
+++ Assets/Scripts/ThrusterConstantForce.cs
@@ -4,18 +4,10 @@ using System.Collections;
 public class ThrusterConstantForce : Thruster 
 {
     [SerializeField]
-    private float m_thrustForce;
+    private Vector2 m_thrustForce = new Vector2(0.0f, 1.0f);
     
-    public override Vector3 ThrustForce()
+    public override Vector2 ThrustForce()
     {
-        Vector3 velocity = m_rigidbody.velocity;
-        Vector3 deltaForce = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            deltaForce += Vector3.up * m_thrustForce;
-        }
-        
-        return deltaForce;
+        return m_thrustForce;
     }
 }
