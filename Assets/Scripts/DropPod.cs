e2ce6d2ba5806673a64e62c5034b8f96027bc201
Andreas <a.mikko92@gmail.com>
2016-02-03 17:13:16 +0100

    Setup for collision detection on pod

--- Assets/Scripts/DropPod.cs
+++ Assets/Scripts/DropPod.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class DropPod : MonoBehaviour 
 {
     [SerializeField]
-    Thruster m_thruster;
+    private Thruster m_thruster;
 
-    private float m_fuel = 1.0f;
+    [SerializeField]
+    private float m_maxLandingVelocity = 1.0f;
+
+    private int m_fuel = 100;
+
+    private bool m_shield = false;
 
     private Transform m_transform;
     private Rigidbody2D m_rigidbody2D;
+    private BoxCollider2D m_boxCollider2D;
 
     private RayShooter2D m_rayShooter2D;
 
@@ -21,6 +27,7 @@ public class DropPod : MonoBehaviour
         m_transform = transform;
         m_rigidbody2D = GetComponent<Rigidbody2D>();
         m_thruster = GetComponent<Thruster>();
+        m_boxCollider2D = GetComponent<BoxCollider2D>();
 	}
 	
 	private void FixedUpdate()
@@ -41,8 +48,102 @@ public class DropPod : MonoBehaviour
         return m_rigidbody2D.velocity.magnitude;
     }
 
-    public float Fuel()
+    public int Fuel()
     {
         return m_fuel;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;
+        string layerName = LayerMask.LayerToName(other.layer);
+
+        if( string.Equals(layerName, "Ground") )
+        {
+            bool land = LandingSequence(collision);
+            if(land)
+            {
+                LevelComplete();
+            }
+            else
+            {
+                KillMe();
+            }
+        }
+        
+        if (string.Equals(layerName, "Platform"))
+        {
+            bool land = LandingSequence(collision);
+            if (!land)
+            {
+                KillMe();
+            }
+        }
+
+        if (string.Equals(layerName, "Enemy"))
+        {
+            if(m_shield)
+            {
+                RemoveShield();
+            }
+            else
+            {
+                KillMe();
+            }
+        }
+    }
+
+    private bool LandingSequence(Collision2D collision)
+    {
+        // TODO: Calculate direction of collision. Ray casting?
+
+        return LandVelocityCheck();
+    }
+
+    private bool LandVelocityCheck()
+    {
+        // Compare Current velocity against maximum allowed landing velocity
+        return (m_maxLandingVelocity * m_maxLandingVelocity) > m_rigidbody2D.velocity.sqrMagnitude; ;
+    }
+
+    private void KillMe()
+    {
+        Debug.Log("BOOM!");
+        // TODO: Explosions and game over event
+    }
+
+    private void LevelComplete()
+    {
+        Debug.Log("Landed!");
+        // TODO: Send level clear event
+    }
+
+    private void RemoveShield()
+    {
+        // Have no shield. Do nothing
+        if (!m_shield)
+            return;
+
+        m_shield = false;
+
+        // TODO: Shield remove effects here
+    }
+
+    private void AddShield()
+    {
+        // Already have shield. Do nothing
+        if (m_shield)
+            return;
+
+        m_shield = true;
+
+        // TODO: Shield activation effects here
+    }
+
+    public Vector3 PodBottomCenter()
+    {
+        Vector3 bottomCenter = transform.position;
+        bottomCenter.y += m_boxCollider2D.size.y * 0.5f;
+        return bottomCenter;
     }
 }
