340599e159fddb94cf76afbf5c763804550008a0
Emil Åhsberg <lovet1991@hotmail.com>
2016-02-10 14:30:10 +0100

    Implemented simple spawn manager and object pool added asteroid with simple behaviour

---
+++ Assets/Scripts/AsteroidBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidBehaviour : MonoBehaviour {
+
+    private Vector3 startPos;
+
+    void OnEnable()
+    {
+       startPos = transform.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.x > 15 || transform.position.x < -15)
+        {
+            gameObject.SetActive(false);
+        }
+
+        else
+        {
+            if (startPos.x <= 0)
+            {
+                transform.Translate(0.1f, 0, 0);
+            }
+
+            else
+            {
+                transform.Translate(-0.1f, 0, 0);
+            }
+        }
+         
+	}
+}
---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleManager : MonoBehaviour {
+
+    //public PlayerAlive playerAlive;
+    [SerializeField]
+    private GameObject obstacle;
+
+    [SerializeField]
+    private GameObject pod;
+
+    [SerializeField]
+    private float spawnTime = 1.0f;
+
+    [SerializeField]
+    private Transform[] spawnPoints;
+
+    private GameObjectPool asteroidOP;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Spawn",spawnTime, spawnTime);
+        asteroidOP = GetComponent<GameObjectPool>();
+	}
+	
+    void Spawn()
+    {
+        if (pod.transform.position.y > 2)
+        {
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            GameObject spawnObject = asteroidOP.GetPooledObject();
+            if (spawnObject != null)
+            {
+                spawnObject.transform.position = spawnPoints[spawnPointIndex].position;
+                spawnObject.SetActive(true);
+            }
+        }
+    }
+}
