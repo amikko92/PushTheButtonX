9d5c9a7ff0efafd6fd2d7ad12a90f54b83c67e03
Mathias Bylund <materbyl@gmail.com>
2016-02-17 14:39:59 +0100

    Created ObjectState and GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum gameState { START, PLAY, WIN, LOSE };
+
+public class GameManager : MonoBehaviour {
+
+    ObjectState[] listeners;
+
+    //--------------------------------------------------------
+
+    protected GameManager() { }
+
+	// Use this for initialization
+	void Awake () {
+            
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
