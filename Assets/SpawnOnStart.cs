6f7d12138f6b6c20d0d2dbaa9ff0d355b88669f8
Emil Åhsberg <lovet1991@hotmail.com>
2016-02-24 15:28:20 +0100

    added functionality to spawn enemies upon game start with options such as continuous spawning and spawning a set amount of enemies at given intervals

---
+++ Assets/SpawnOnStart.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOnStart : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool continuousSpawning;
+
+    [SerializeField]
+    private SpawnManager spawnManager;
+
+    [SerializeField]
+    private int nbrToSpawn;
+
+    [SerializeField]
+    private float spawnDelayOffset;
+
+    [SerializeField]
+    private float spawnDelay;
+
+    [SerializeField]
+    private SpawnableType spawnType;
+
+    private float time = 0;
+
+    private GameObject spawn;
+
+    private int nbrSpawned;
+
+    void Awake()
+    { 
+        time = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (continuousSpawning && Time.time - time > spawnDelay)
+        {
+            PointSpawn();
+        }
+
+        else if (Time.time - time > spawnDelay && nbrSpawned < nbrToSpawn)
+        {
+            PointSpawn();
+            nbrSpawned++;
+        }
+    }
+
+    public void PointSpawn()
+    {
+
+        GameObject spawn = spawnManager.ManagerSpawn(spawnType);
+        if (spawn != null)
+        {
+            spawn.transform.position = transform.position;
+            spawn.SetActive(true);
+            time = Time.time + Random.Range(-spawnDelayOffset, spawnDelayOffset);
+        }
+    }
+}
