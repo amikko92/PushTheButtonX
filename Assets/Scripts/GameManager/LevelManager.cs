2a2cbeab586a4c8d340073c69e2ea36cc649d918
Andreas <a.mikko92@gmail.com>
2016-02-24 15:15:40 +0100

    Build scenes

--- Assets/Scripts/GameManager/LevelManager.cs
+++ Assets/Scripts/GameManager/LevelManager.cs
@@ -4,11 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public void ChangeLevel()
+    public void LoadNextLevel()
     {
         GameManager.Instance.Nullify(true);
-        SceneManager.LoadScene("New E-man");
+        int curScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(curScene + 1);
     }
+
+    public void LoadLevel(int buildIndex)
+    {
+        GameManager.Instance.Nullify(true);
+        SceneManager.LoadScene(buildIndex);
+    }
+
     public void RedoLevel()
     {
         GameManager.Instance.Nullify(true);
