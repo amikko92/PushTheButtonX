adfd18683a8d3505718c57ef0237f0c0b341d401
Andreas <a.mikko92@gmail.com>
2016-02-24 13:56:09 +0100

    Merge -gui null refs -collisions

--- Assets/Scripts/GUI/GUIHandler.cs
+++ Assets/Scripts/GUI/GUIHandler.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class GUIHandler : MonoBehaviour {
 
-    public DropPod pod;
+    private DropPod m_pod;
+    public DropPod pod
+    {
+        get { return m_pod; }
+    }
 
 	// Use this for initialization
 	void Awake () {
-
-	}
+        GameObject go = GameObject.Find("Pod");
+        m_pod = go.GetComponent<DropPod>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
