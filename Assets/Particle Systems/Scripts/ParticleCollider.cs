1309c18c38716fee8eb858448e086cb062c8a52c
Mathias Bylund <materbyl@gmail.com>
2016-03-02 17:48:59 +0100

    Updated and changed the explosive cloud to use OnTriggerStay2D instead of a coroutine. Updated method calls.

--- Assets/Particle Systems/Scripts/ParticleCollider.cs
+++ Assets/Particle Systems/Scripts/ParticleCollider.cs
@@ -7,14 +7,14 @@ public class ParticleCollider : MonoBehaviour {
     public Material aliveMTRL, destroyMTRL;
 
     private ParticleSystem ps;
-    private Renderer r;
+    private Renderer renderer;
     private IEnumerator coroutine;
 
 	// Use this for initialization
 	void Awake () {
         ps = GetComponent<ParticleSystem>();
-        r = GetComponent<Renderer>();
-        r.material = aliveMTRL;
+        renderer = GetComponent<Renderer>();
+        renderer.material = aliveMTRL;
     }
 	
 	// Update is called once per frame
@@ -24,28 +24,47 @@ public class ParticleCollider : MonoBehaviour {
 
     void OnTriggerEnter2D (Collider2D col)
     {
-        coroutine = CheckVelocity(col);
-        StartCoroutine(coroutine);
+        //coroutine = CheckVelocity(col);
+        //StartCoroutine(coroutine);
+    }
+
+    void OnTriggerStay2D (Collider2D col)
+    {
+        string objLayerName = LayerMask.LayerToName(col.gameObject.layer);
+        if (string.Equals(objLayerName, "Player"))
+        {
+            var pod = col.GetComponent<DropPod>();
+            if (pod.Velocity() > 0)
+            {
+                renderer.material = destroyMTRL;
+                Fade();
+                pod.SendMessage("EnemyHit");
+            }
+        }
     }
 
     void OnTriggerExit2D (Collider2D col)
     {
-        StopCoroutine(coroutine);
+        //StopCoroutine(coroutine);
     }
 
     IEnumerator CheckVelocity (Collider2D col)
     {
-        while (true)
+        while(true)
         {
-            var player = col.GetComponent<Rigidbody2D>();
-            if (player.velocity.y > 0)
+            string objLayerName = LayerMask.LayerToName(col.gameObject.layer);
+            if (string.Equals(objLayerName, "Player"))
             {
-                r.material = destroyMTRL;
-                Fade();
-                player.SendMessage("KillMe");
-                yield return false;
+                var pod = col.GetComponent<DropPod>();
+                if (pod.Velocity() > 0)
+                {
+                    renderer.material = destroyMTRL;
+                    Fade();
+                    pod.SendMessage("EnemyHit");
+                    yield return false;
+                }
+                yield return null;
             }
-            yield return null;
         }
         
     }
