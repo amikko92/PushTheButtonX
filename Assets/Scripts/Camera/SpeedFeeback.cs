0b207a55b696dd51b6b147c47f0de937a41c0d35
Mathias Bylund <materbyl@gmail.com>
2016-03-02 18:37:45 +0100

    Created SpeedFeedback script which adds motion blur when the pod is moving to fast

---
+++ Assets/Scripts/Camera/SpeedFeeback.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class SpeedFeeback : MonoBehaviour {
+
+    [SerializeField]
+    private DropPod pod;
+
+    [SerializeField]
+    private float scale;
+
+    private CameraMotionBlur motionBlur;
+
+	// Use this for initialization
+	void Awake () {
+        motionBlur = GetComponent<CameraMotionBlur>();
+
+        // Make sure that the velocity starts at zero
+        motionBlur.velocityScale = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Mathf.Abs(pod.Velocity()) > pod.GetMaxLandingVelocity()) motionBlur.velocityScale = scale * (Mathf.Abs(pod.Velocity()) - pod.GetMaxLandingVelocity());
+        else motionBlur.velocityScale = 0;
+
+        clamp();
+    }
+
+    private void clamp()
+    {
+        if (motionBlur.velocityScale > motionBlur.maxVelocity) motionBlur.velocityScale = motionBlur.maxVelocity;
+        else if (motionBlur.velocityScale < motionBlur.minVelocity) motionBlur.velocityScale = motionBlur.minVelocity;
+    }
+}
--- Assets/Scripts/Pod/DropPod.cs
+++ Assets/Scripts/Pod/DropPod.cs
@@ -420,4 +420,9 @@ public class DropPod : MonoBehaviour
             }
         }       
     }
+
+    public float GetMaxLandingVelocity()
+    {
+        return m_maxLandingVelocity;
+    }
 }
